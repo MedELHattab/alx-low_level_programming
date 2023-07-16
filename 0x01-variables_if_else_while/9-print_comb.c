35dd24f1c668795b0ff339927ca091a3cee6f221
MedElHattab <elhattabmohammedelarbi@gmail.com>
2023-07-16 09:46:18 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 /**
 * main - prints all possible combinations of single-digit numbers
-* followed by a new line
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 int n;
-char ch;
 for (n = 48; n < 58; n++)
 {
 putchar(n);
 if (n != 57)
 {
 putchar(',');
-putchar('');
+putchar(' ');
 }
 }
 putchar('\n');
